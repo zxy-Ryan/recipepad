17f4f4c2366a6d4966f2268197e097f1ea3c072d
tianyuanzoe <tian.yuan3@northeastern.edu>
2023-12-15 01:28:22 -0800

    comment css add

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,25 +1,27 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 // import { v4 as uuidv4 } from 'uuid';
 import axios from "axios";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
+import { FaRegUser } from "react-icons/fa";
+import { MdDateRange } from "react-icons/md";
+import { FaRegCommentDots } from "react-icons/fa";
 
 export const BASE_API = process.env.REACT_APP_BASE_API_URL;
 export const USERS_API = `${BASE_API}/api/components`;
-
 
 const RecipeDetails = ({ match }) => {
   const { recipeId } = useParams();
   // const recipeId='52772'
   const [recipe, setRecipe] = useState(null);
-  const [commentContent, setCommentContent] = useState('');
+  const [commentContent, setCommentContent] = useState("");
   const [comments, setComments] = useState([]);
   const REMOTE_URL = "https://recipepad-server.onrender.com";
   const LOCAL_URL = "http://localhost:5000";
-  const BASE_URL = REMOTE_URL||LOCAL_URL;
+  const BASE_URL = REMOTE_URL || LOCAL_URL;
   // const BASE_URL = LOCAL_URL;
   const URL = `${BASE_URL}/api/comments`;
-  const URLSaved =  `${BASE_URL}/api/comments/saved`;
+  const URLSaved = `${BASE_URL}/api/comments/saved`;
   // const [user, setUser] = useState({
   //   id: '656707bb65aba2b8a9f430f8',
   //   // username: 'example_user',
@@ -30,10 +32,9 @@ const RecipeDetails = ({ match }) => {
   // const user = useSelector(state => state.auth.user);
   const { user } = useSelector((state) => ({ ...state.auth }));
 
-  const userId = user?.result?._id
-  const userName = user?.result?.name
-  const role = user?.result?.role
-
+  const userId = user?.result?._id;
+  const userName = user?.result?.name;
+  const role = user?.result?.role;
 
   const getCommentsForRecipe = async (recipeId) => {
     const response = await axios.get(`${URL}/${recipeId}`);
@@ -45,62 +46,65 @@ const RecipeDetails = ({ match }) => {
     return response.data;
   };
 
-//   const createComment = async (commentData) => {
-//     const response = await axios.post(URL, commentData);
-//     // return response.data;
-//     return response.status;
-// };
+  //   const createComment = async (commentData) => {
+  //     const response = await axios.post(URL, commentData);
+  //     // return response.data;
+  //     return response.status;
+  // };
   const createComment = async (commentData) => {
     try {
       const response = await axios.post(URL, commentData);
-      return response.status === 200; 
+      return response.status === 200;
     } catch (error) {
-      console.error('Failed to post comment:', error);
+      console.error("Failed to post comment:", error);
       return false;
     }
-};
+  };
 
-const formatDate = (dateString) => {
-  return dateString.substring(0, 10); 
-};
+  const formatDate = (dateString) => {
+    return dateString.substring(0, 10);
+  };
 
-// const updateSavedRecipe = async (userId, newRecipe) => {
-//   try {
-//     const updateSavedRecipe = await axios.post(`${URL}/update/${userId}`, userId, newRecipe);
-//     // return response.status === 200;
-//     return updateSavedRecipe
-//   } catch (error) {
-//     console.error('Failed to update savedRecipe:', error);
-//     return false; 
-//   }
-// };
+  // const updateSavedRecipe = async (userId, newRecipe) => {
+  //   try {
+  //     const updateSavedRecipe = await axios.post(`${URL}/update/${userId}`, userId, newRecipe);
+  //     // return response.status === 200;
+  //     return updateSavedRecipe
+  //   } catch (error) {
+  //     console.error('Failed to update savedRecipe:', error);
+  //     return false;
+  //   }
+  // };
 
-const updateSavedRecipe = async (newRecipe, userId) => {
-  try {
-    const response = await axios.put(`${REMOTE_URL}/update/${userId}`, newRecipe);
-    return response
-  } catch (error) {
-    console.error('Failed to update savedRecipe:', error);
-  }
+  const updateSavedRecipe = async (newRecipe, userId) => {
+    try {
+      const response = await axios.put(
+        `${REMOTE_URL}/update/${userId}`,
+        newRecipe
+      );
+      return response;
+    } catch (error) {
+      console.error("Failed to update savedRecipe:", error);
+    }
+  };
 
-}
-
-  
   useEffect(() => {
     const fetchRecipeAndComments = async () => {
       try {
         // 获取菜谱信息
-        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`);
+        const response = await fetch(
+          `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`
+        );
         const data = await response.json();
         setRecipe(data.meals[0]);
         const recipeSaved = await isRecipeSaved();
-        setIsSaved(recipeSaved); 
+        setIsSaved(recipeSaved);
         // 获取评论信息
         const commentsData = await getCommentsForRecipe(recipeId);
         setComments(commentsData.comments);
         scrollToComment();
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
@@ -118,37 +122,32 @@ const updateSavedRecipe = async (newRecipe, userId) => {
     };
 
     fetchRecipeAndComments();
-  },  [recipeId,userId]);
+  }, [recipeId, userId]);
 
-  
-
-  
-  const handlePostComment = async () =>{
+  const handlePostComment = async () => {
     // const userId = user.id;
     // const userId = userId;
-    // const commentId = uuidv4(); 
+    // const commentId = uuidv4();
     const commentData = {
-        // commentId: commentId,
-        userId: userId,
-        userName: userName,
-        recipeId: recipeId, 
-        commentContent: commentContent,
-        time: new Date().toISOString(), 
+      // commentId: commentId,
+      userId: userId,
+      userName: userName,
+      recipeId: recipeId,
+      commentContent: commentContent,
+      time: new Date().toISOString(),
     };
     try {
-        const response = await createComment(commentData)
-        if (response) {
-        
-        setCommentContent(''); 
-        
-        setComments([...comments,commentData])
-        } else {
+      const response = await createComment(commentData);
+      if (response) {
+        setCommentContent("");
 
-          console.error('Failed to post comment');
-        }
-      } catch (error) {
-        console.error('Error posting comment:', error);
+        setComments([...comments, commentData]);
+      } else {
+        console.error("Failed to post comment");
       }
+    } catch (error) {
+      console.error("Error posting comment:", error);
+    }
   };
 
   // useEffect(() => {
@@ -168,133 +167,172 @@ const updateSavedRecipe = async (newRecipe, userId) => {
   //   }
   // }, [user.Id]);
 
-  const isRecipeSaved = async () =>{
+  const isRecipeSaved = async () => {
     try {
       // const userId = user.id;
-      
+
       // console.log(userId)
       console.log(userId);
       const savedRecipe = await getSavedRecipe(userId);
       const isRecipeSaved = savedRecipe.user.saveRecipe.includes(recipeId);
       return isRecipeSaved;
     } catch (error) {
-      console.error('Error fetching savedRecipe:', error);
+      console.error("Error fetching savedRecipe:", error);
     }
-
   };
 
-
-
-  const handleSave = async () =>{
+  const handleSave = async () => {
     if (user) {
       if (isSaved) {
-       
-        
-
         // const savedRecipe = await getSavedRecipe(user.id);
         const savedRecipe = await getSavedRecipe(userId);
-        
-        const updatedSaveRecipe = savedRecipe.user.saveRecipe.filter(id => id !== recipeId);
-        savedRecipe.user.saveRecipe = updatedSaveRecipe
+
+        const updatedSaveRecipe = savedRecipe.user.saveRecipe.filter(
+          (id) => id !== recipeId
+        );
+        savedRecipe.user.saveRecipe = updatedSaveRecipe;
         // const response = await updateSavedRecipe(savedRecipe.user.saveRecipe, user.id)
-        const response = await updateSavedRecipe(savedRecipe.user.saveRecipe, userId)
-        
-        
-        
+        const response = await updateSavedRecipe(
+          savedRecipe.user.saveRecipe,
+          userId
+        );
+
         setIsSaved(!isSaved);
-        
       } else {
-       
         // const savedRecipe = await getSavedRecipe(user.id);
         const savedRecipe = await getSavedRecipe(userId);
         console.log(userId);
-        const updatedSaveRecipe = savedRecipe.user.saveRecipe.filter(id => id !== recipeId);
-        
+        const updatedSaveRecipe = savedRecipe.user.saveRecipe.filter(
+          (id) => id !== recipeId
+        );
+
         savedRecipe.user.saveRecipe = [...updatedSaveRecipe, recipeId];
         // const response = await updateSavedRecipe(savedRecipe.user.saveRecipe, user.id);
-        const response = await updateSavedRecipe(savedRecipe.user.saveRecipe, userId)
-                setIsSaved(!isSaved);
+        const response = await updateSavedRecipe(
+          savedRecipe.user.saveRecipe,
+          userId
+        );
+        setIsSaved(!isSaved);
       }
     } else {
-      console.log('User is not logged in');
-  
+      console.log("User is not logged in");
     }
   };
   // console.log("t1")
   // console.log(Object.values(comments))
 
   return (
-    <div >
-    <div className="container mt-4" style={{ textAlign: 'left' }}>
-      {recipe ? (
-        <div className="row">
-          <div className="col-md-6 mb-4">
-            <img src={recipe.strMealThumb} alt={recipe.strMeal} className="img-fluid" />
+    <div>
+      <div className="container" style={{ textAlign: "left" }}>
+        {recipe ? (
+          <div className="row">
+            <div className="col-md-6 mb-4">
+              <img
+                src={recipe.strMealThumb}
+                alt={recipe.strMeal}
+                className="img-fluid"
+              />
+            </div>
+            <div className="col-md-6">
+              <h2>{recipe.strMeal}</h2>
+              <p>
+                {recipe.strArea} | {recipe.strTags}
+              </p>
+              <h4>Ingredients</h4>
+              <ul>
+                {Object.keys(recipe)
+                  .filter((key) => key.includes("strIngredient") && recipe[key])
+                  .map((ingredientKey, index) => (
+                    <li key={index}>
+                      {recipe[ingredientKey]} -{" "}
+                      {recipe[`strMeasure${ingredientKey.slice(13)}`]}
+                    </li>
+                  ))}
+              </ul>
+              <h4>Instructions</h4>
+              <p>{recipe.strInstructions}</p>
+            </div>
           </div>
-          <div className="col-md-6">
-            <h2>{recipe.strMeal}</h2>
-            <p>{recipe.strArea} | {recipe.strTags}</p>
-            <h4>Ingredients</h4>
-            <ul>
-              {Object.keys(recipe)
-                .filter(key => key.includes('strIngredient') && recipe[key])
-                .map((ingredientKey, index) => (
-                  <li key={index}>{recipe[ingredientKey]} - {recipe[`strMeasure${ingredientKey.slice(13)}`]}</li>
-                ))}
-            </ul>
-            <h4>Instructions</h4>
-            <p>{recipe.strInstructions}</p>
+        ) : (
+          <p>Loading recipe...</p>
+        )}
+        {(role === "admin" || role === "vip") && (
+          <div className="mb-4">
+            <button
+              className="btn btn-primary"
+              onClick={handleSave}
+              style={{ backgroundColor: "black" }}
+            >
+              {isSaved ? "Unsave" : "Save"}
+            </button>
+          </div>
+        )}
+
+        {user && (
+          <div className="mb-4">
+            <h4>Comments</h4>
+            <textarea
+              rows="4"
+              className="form-control mb-2"
+              placeholder="Write your comment here..."
+              value={commentContent}
+              onChange={(e) => setCommentContent(e.target.value)}
+            ></textarea>
+            <button
+              className="btn btn-primary"
+              onClick={handlePostComment}
+              style={{ backgroundColor: "black" }}
+            >
+              Post
+            </button>
+          </div>
+        )}
+
+        <div class="container my-5 py-5">
+          <div class="row d-flex">
+            <div class="col-md-12 col-lg-10">
+              <div class="card text-dark">
+                {user &&
+                  comments.map((comment, index) => (
+                    <>
+                      <div class="card-body p-4">
+                        {index === 0 && (
+                          <>
+                            <h4 className="mb-0">Recent comments</h4>
+                            <p className="fw-light mb-4 pb-2">
+                              Latest Comments section by users
+                            </p>
+                          </>
+                        )}
+                        <div class="d-flex flex-start">
+                          <div>
+                            <div className="d-flex align-items-center">
+                              <FaRegUser className="me-2" />
+                              <Link className="fw-bold mb-1" to={`/Profile/${comment.userId}`}>
+                                {comment.userName}
+                              </Link>
+                            </div>
+          
+                            <div class="d-flex align-items-center mb-3">
+                            <MdDateRange className="me-2"/>
+                              <p class="mb-0">{formatDate(comment.time)}</p>
+                            </div>
+                            <div className="d-flex align-items-center">
+                            <FaRegCommentDots className="me-2" />
+                            <p class="mb-0">{comment.commentContent}</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                      <hr class="my-0" />
+                    </>
+                  ))}
+              </div>
+            </div>
           </div>
         </div>
-      ) : (
-        <p>Loading recipe...</p>
-      )}
-       {(role === 'admin' || role==='vip') && (
-        <div className="mb-4">
-          <button className="btn btn-primary" onClick={handleSave} style={{ backgroundColor:'black'}}>
-            {isSaved ? 'Unsave' : 'Save'}
-          </button>
-        </div>
-      )}
-
-
-      {user && (
-        <div className="mb-4">
-          <h4>Comments</h4>
-          <textarea
-            rows="4"
-            className="form-control mb-2"
-            placeholder="Write your comment here..."
-            value={commentContent}
-            onChange={(e) => setCommentContent(e.target.value)}
-          ></textarea>
-          <button className="btn btn-primary" onClick={handlePostComment} style={{ backgroundColor:'black'}}>
-            Post
-          </button>
-        </div>
-      )}
-      
-
-      {/* 显示已有的评论 */}
-      <div className="mb-4">
-        <ul style={{listStyle:'none'}}>
-{user && comments.map((comment, index) => (
-      <li key={index} id={`comment-${comment._id}`}>
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <Link to={`/Profile/${comment.userId}`}>
-              {/* <span style={{ marginRight: '10px' }}>{comment.userId}</span> */}
-              <span style={{ marginRight: '10px' }}>{comment.userName}</span>
-            </Link>
-            <span>{formatDate(comment.time)}</span>
-          </div>
-          <p>{comment.commentContent}</p>
-        </div>
-      </li>
-    ))}
-        </ul>
       </div>
-    </div>
     </div>
   );
 };
